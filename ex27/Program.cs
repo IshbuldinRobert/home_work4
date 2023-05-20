1f682a798524831ec3a55c7f9c087b2f6a925276
Robert <ishbuldinrobert2002@gmail.com>
2023-05-20 11:20:47 +0500

    home work done

---
+++ ex27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27:
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int FindSum(int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        res = res + n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+if (num > 0)
+{
+    Console.WriteLine($"Сумма цифр в числе {num} равна {FindSum(num)}");
+}
+else Console.WriteLine("Число должно быть положительным (-_-)");
---
+++ ex29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] mass = new int[8];
+
+void FillArr(int[] fillArr)
+{
+    for (int i = 0; i < fillArr.Length; i++)
+    {
+        fillArr[i] = new Random().Next(10, 100);
+    }
+}
+
+void PrintArr(int[] printArr)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArr.Length; i++)
+    {
+        if (i == printArr.Length - 1) Console.WriteLine($"{printArr[i]}]");
+        else Console.Write($"{printArr[i]}, ");
+    }
+}
+
+FillArr(mass);
+PrintArr(mass);
